de6bf3ee03fd5b8adcd43840acd75ab29798d82f
DaKnig <37626476+DaKnig@users.noreply.github.com>
2023-06-04 17:42:26 +0300

    Made Window into its own composite widget.
    next step is cleaning the code ... and the file names and their placement.

---
+++ src/ui/window.rs
@@ -0,0 +1,117 @@
+use adw::{gio, glib, gtk, prelude::*, subclass::prelude::*, Application};
+use glib::Object;
+
+use gtk::{NoSelection, SignalListItemFactory};
+
+mod imp;
+use crate::ui::payment::{Payment, PaymentWidget};
+
+use crate::ui::transaction::{Transaction, TransactionWidget};
+
+glib::wrapper! {
+    pub struct Window(ObjectSubclass<imp::Window>)
+        @extends adw::ApplicationWindow, gtk::ApplicationWindow,
+                 gtk::Window, gtk::Widget,
+        @implements gio::ActionGroup, gio::ActionMap, gtk::Accessible,
+                    gtk::Buildable, gtk::ConstraintTarget, gtk::Native,
+                    gtk::Root, gtk::ShortcutManager;
+}
+
+impl Window {
+    pub fn new(app: &Application) -> Self {
+        // Create new window
+        let this: Self =
+            Object::builder().property("application", app).build();
+
+        // now create the list thingie:
+        // view
+        // model
+        let input_list_store = this.imp().input_list_store.clone();
+        let input_selection_model =
+            NoSelection::new(Some(input_list_store));
+        let input_view = this.imp().input_view.get();
+        input_view.set_model(Some(&input_selection_model));
+
+        let output_list_store = this.imp().output_list_store.clone();
+        let output_selection_model =
+            NoSelection::new(Some(output_list_store));
+        let output_view = this.imp().output_view.get();
+        output_view.set_model(Some(&output_selection_model));
+
+        // factory
+        let input_factory = SignalListItemFactory::new();
+        input_factory.connect_setup(move |_, list_item| {
+            let widget = PaymentWidget::new();
+            list_item.set_child(Some(&widget));
+        });
+
+        input_factory.connect_bind(move |_, list_item| {
+            // Get `Payment` from `ListItem`
+            let payment: Payment = list_item
+                .item()
+                .and_downcast()
+                .expect("The item has to be an `Payment`.");
+
+            // Get `PaymentWidget` from `ListItem`
+            let widget: PaymentWidget = list_item
+                .child()
+                .and_downcast()
+                .expect("The child has to be a `PaymentWidget`.");
+
+            // Set "widget" to "payment"
+            widget.bind_payment(payment);
+        });
+
+        input_factory.connect_unbind(move |_, list_item| {
+            // Get `PaymentWidget` from `ListItem`
+            let widget: PaymentWidget = list_item
+                .child()
+                .and_downcast()
+                .expect("The child has to be a `PaymentWidget`.");
+
+            // unbind
+            widget.unbind_payment();
+        });
+
+        input_view.set_factory(Some(&input_factory));
+
+        let output_factory = SignalListItemFactory::new();
+
+        output_factory.connect_setup(move |_, list_item| {
+            list_item.set_child(Some(&TransactionWidget::new()));
+        });
+
+        output_factory.connect_unbind(move |_, list_item| {
+            let widget: TransactionWidget = list_item
+                .child()
+                .and_downcast()
+                .expect("The child has to be a `TransactionWidget`.");
+
+            // unbind
+            widget.unbind_transaction();
+        });
+
+        output_factory.connect_bind(move |_, list_item| {
+            // Get `Transaction` from `ListItem`
+            let transaction: Transaction = list_item
+                .item()
+                .and_downcast()
+                .expect("The item has to be an `Transaction`.");
+
+            // Get `TransactionWidget` from `ListItem`
+            let widget: TransactionWidget = list_item
+                .child()
+                .and_downcast()
+                .expect("The child has to be a `TransactionWidget`.");
+
+            // Set "widget" to "transaction"
+            widget.bind_transaction(transaction);
+        });
+        output_view.set_factory(Some(&output_factory));
+
+        // splitting...
+        // let split_button: Button = this.imp().split_button.get();
+        // split_button.connect_clicked(imp::Window::split);
+        this
+    }
+}
---
+++ src/ui/window/imp.rs
@@ -0,0 +1,129 @@
+use std::collections::{BTreeMap, VecDeque};
+
+use adw::{gio, glib, gtk, prelude::*, subclass::prelude::*};
+use gio::ListStore;
+use glib::subclass::InitializingObject;
+use gtk::{Button, CompositeTemplate, ListView};
+
+use crate::ui::{payment::*, transaction::*};
+
+// Object holding the state
+#[derive(CompositeTemplate, Default)]
+#[template(resource = "/null/daknig/pay-split-rs/window.ui")]
+pub struct Window {
+    #[template_child(id = "input-view")]
+    pub input_view: TemplateChild<ListView>,
+    #[template_child(id = "output-view")]
+    pub output_view: TemplateChild<ListView>,
+    #[template_child(id = "split-button")]
+    pub split_button: TemplateChild<Button>,
+
+    pub input_list_store: ListStore,
+    pub output_list_store: ListStore,
+}
+
+// The central trait for subclassing a GObject
+#[glib::object_subclass]
+impl ObjectSubclass for Window {
+    // `NAME` needs to match `class` attribute of template
+    const NAME: &'static str = "PaySplitWindow";
+    type Type = super::Window;
+    type ParentType = adw::ApplicationWindow;
+
+    fn class_init(klass: &mut Self::Class) {
+        klass.bind_template();
+        klass.bind_template_callbacks();
+    }
+
+    fn instance_init(obj: &InitializingObject<Self>) {
+        obj.init_template();
+    }
+}
+
+impl ObjectImpl for Window {}
+impl WidgetImpl for Window {}
+impl WindowImpl for Window {}
+impl ApplicationWindowImpl for Window {}
+impl AdwApplicationWindowImpl for Window {}
+
+#[gtk::template_callbacks]
+impl Window {
+    #[template_callback]
+    pub fn add_row(&self, _: &Button) {
+        let payment = super::Payment::default();
+        self.input_list_store.append(&payment);
+    }
+    #[template_callback]
+    pub fn split(&self, _: &Button) {
+        let mut paid = BTreeMap::new();
+        let mut total: f32 = 0.;
+        for payment in &self.input_list_store {
+            let payment = payment
+                .ok() // safely since we wont change the list store
+                .and_downcast::<Payment>()
+                .expect("The item has to be a `Payment`.");
+
+            *paid.entry(payment.from()).or_insert(0.) += payment.amount();
+            total += payment.amount();
+        }
+        if total.is_nan() {
+            eprintln!("please correct your inputs");
+            return;
+        }
+
+        if let Some(x) = paid.get("") {
+            if x.abs() < EPSILON {
+                paid.remove("");
+            }
+        }
+        let avg = total / (paid.len() as f32);
+        // using partial_cmp to ensure no NANs and such.
+        let mut paid: Vec<_> = paid.iter().collect();
+        paid.sort_unstable_by(|a, b| a.1.partial_cmp(b.1).unwrap());
+
+        const EPSILON: f32 = 0.01;
+        // normalize towards the average
+        let mut paid: VecDeque<_> =
+            paid.into_iter().map(|x| (x.1 - avg, x.0)).collect();
+
+        self.output_list_store.remove_all();
+        //output_list_store; // Vec::new();
+
+        println!("debug: normalized paid list");
+        for payment in &paid {
+            println!("{} -> {}$", payment.1, payment.0)
+        }
+
+        // tricking rust into giving me the front and the back :)
+        while let (Some(mut front), Some(back)) =
+            (paid.pop_front(), paid.back_mut())
+        {
+            // removes all the tiny leftovers
+            if -front.0 <= EPSILON {
+                continue;
+            } else if back.0 < EPSILON {
+                paid.pop_back();
+                paid.push_front(front);
+                continue;
+            }
+
+            // amount to transfer
+            let amount = back.0.min(front.0.abs());
+            // transfer
+            self.output_list_store
+                .append(&Transaction::new(&front.1, &back.1, amount));
+            front.0 += amount;
+            back.0 -= amount;
+
+            // prepare for next iteration
+            paid.push_front(front);
+        }
+        // by now we have drained the list
+        println!("debug: normalized paid list");
+        for debt in &self.output_list_store {
+            let debt: Transaction =
+                debt.ok().and_downcast::<Transaction>().unwrap();
+            println!("{}", debt);
+        }
+    }
+}
